302057948ebf9ba5a7effdfe2c8f3c0fc5d21baa
kumasan3618 <s.kumasaka0329@gmail.com>
2021-08-14 14:22:09 +0900

    ホームの作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,32 +30,15 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      
+        
+      
+        
+      
+      
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      
     </v-app-bar>
     <v-main>
       <v-container>
@@ -98,20 +81,26 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'kingyo',
+          to: '/kingyo'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: '夏祭り'
     }
   }
 }
 </script>
+
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,29 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-card class="pa-4">
+    <v-card-title class="font-weight-bold red lighten-2">夏祭りミニゲーム</v-card-title>
+    <v-card-text>夏祭りの屋台ゲームが盛りだくさん！</v-card-text>
+    <v-card-title>稼働する条件や、空き状況</v-card-title>
+    </v-card>
+    <v-layout class="pt-4">
+      <v-flex md6 lg6>
+        <v-layout justify-content:center>
+          <v-card class="pa-4">
+            <v-card-title class="font-weight-bold">金魚すくい</v-card-title>
+            <v-card-text>金魚をねらってすくってみよう。ゲットするとポイントがもらえるよ！</v-card-text>
+            <p>
+            <a href="/kingyo">
+              <img src="https://3.bp.blogspot.com/-7RdvF7W1rQ4/VKTgoGDmVsI/AAAAAAAAqVk/pgragsSDFGI/s180-c/pyoko08_kingyosukui.png">
+              金魚すくい
+            </a>
+            </p>
+          </v-card>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<style>
+
+</style>
